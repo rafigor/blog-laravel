7ed0084f481925e071425622af90f038690bfed9
Rafael Oliveira <rafaeligor152@gmail.com>
2016-03-28 23:50:15 -0300

    Blog Laravel

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,8 +13,6 @@
 
 Route::group(['middleware' => ['web']], function () {
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
+    Route::get('/', 'PostController@index');
 
 });
